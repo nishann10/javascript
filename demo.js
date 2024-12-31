4ce41e0a6e54be54d524cd6c0204637c58f6b943
nishann10 <nishannichu54@.com>
2024-12-31 11:02:32 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,99 @@
+// {
+//     let a=4;
+//     let b=6;
+//     sum=a+b;
+//     console.log(sum);   
+// }
+
+// var a=4;
+// if(a==1){
+//     console.log("sunday");
+// }
+// else if(a==2){
+//     console.log("monday");
+// }
+// else if(a==3){
+//     console.log("tuesday");
+// }
+// else if(a==4){
+//     console.log("wednesday")
+// }
+// else if(a==5){
+//     console.log("thersday")
+// }
+// else if(a==6){
+//     console.log("friday")
+// }
+// else if(a==7){
+//     console.log("saturday")
+// }
+// else{
+//     console.log("beyond limit")
+// }
+
+
+
+// var a=5
+// if(a%2==0){
+//     console.log("its even")
+// }
+// else{
+//     console.log("its odd")
+// }
+
+
+
+// var a="o"
+// if(a=="a"||a=="i"||a=="o"||a=="e"||a=="u"){
+//     console.log("its vowel")
+// }
+// else{
+//     console.log("its consonents")
+// }
+
+
+// var a=20
+// if(a%5==0||a%11==0){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+
+// var a=15
+// var b=12
+// var c=15
+// if(a==b&&a==c){
+//     console.log("equileteral")
+// }
+// else if(a==b||b==c||a==c){
+//     console.log("iscoselers")
+// }
+// else{
+//     console.log("scalene")
+// }
+
+// a=1930
+// if(a%100==0){
+//     console.log("its a century year")
+// }
+// else{
+//     console.log("its not a century year")
+// }
+
+
+// x=3
+// y=-4
+// if(x>0&&y>0){
+//     console.log("1 quadrent")
+// }
+// else if(x<0&&y>0){
+//     console.log("2 quadrent")
+// }
+// else if(x<0&&y<0){
+//     console.log("3 quadrent")
+// }
+// else{
+//     console.log("4 quadrent")
+// }
